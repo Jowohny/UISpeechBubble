a56995e7e4ee3357a54cb9b604089bca0f615913
Jowohny <vujohny109238@gmail.com>
2026-06-12 19:53:32 -0700

    Initial Commit

---
+++ UISpeechBubble/UISpeechBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISpeechBubbleApp.swift
+//  UISpeechBubble
+//
+//  Created by Johny Vu on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct UISpeechBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
